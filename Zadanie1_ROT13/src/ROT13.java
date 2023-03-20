d954d44cb00a218e09d348f159e8323600bc3484
Aspink <aspink1@wp.pl>
2023-03-20 21:35:51 +0100

    Method rot13 created and tested in ROT13.

--- Zadanie1_ROT13/src/ROT13.java
+++ Zadanie1_ROT13/src/ROT13.java
@@ -2,6 +2,22 @@ public class ROT13 {
     public static void main(String[] args) {
         String string = "To zdanie jest zakodowane.";
         String encrypted = rot13(string);
+        System.out.println(string);
         System.out.println(encrypted);
+        System.out.println(rot13(encrypted));
+    }
+
+    public static String rot13(String text) {
+        char[] chars = text.toCharArray();
+        String result = "";
+        for (char letter : chars) {
+            if ((letter >= 'a' && letter <= 'm') || (letter >= 'A' && letter <= 'M')) {
+                letter += 13;
+            } else if ((letter >= 'n' && letter <= 'z') || (letter >= 'N' && letter <= 'Z')) {
+                letter -= 13;
+            }
+            result = result.concat(Character.toString(letter));
+        }
+        return result;
     }
 }
