47c4219b4a99d20d9ad0f3788e2456a56ca796c1
Aspink <aspink1@wp.pl>
2023-03-21 13:20:27 +0100

    Method main() created in CaesarsCode class. Class tested.

--- Zadanie2_SzyfrCezara/src/CaesarsCode.java
+++ Zadanie2_SzyfrCezara/src/CaesarsCode.java
@@ -1,11 +1,20 @@
 public class CaesarsCode {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        String string = "To zdanie jest zakodowane.";
+        int shift1 = 13;
+        int shift2 = 6;
+        int shift3 = 7;
+        System.out.println("Tekst początkowy: " + string);
+        String string1 = ceasarsCode(shift1, string);
+        System.out.println("Tekst początkowy po zaszyfrowaniu (przesunięcie: " + shift1 + ") :" + string1);
+        String string2 = ceasarsCode(shift2, string1);
+        System.out.println("Tekst powyżej po zaszyfrowaniu    (przesunięcie:  " + shift2 + ") :" + string2);
+        String string3 = ceasarsCode(shift3, string2);
+        System.out.println("Tekst powyżej po zaszyfrowaniu    (przesunięcie:  " + shift3 + ") :" + string3);
     }
     public static String ceasarsCode(int shift, String text) {
         StringBuilder codedText = new StringBuilder();
-        char[] chars = text.toCharArray();
+        char[] chars = text.toLowerCase().toCharArray();
         for (char letter : chars) {
             if(Character.isLetter(letter)) {
                 letter = (char) (((letter + shift - 'a') % 26) + 'a');
