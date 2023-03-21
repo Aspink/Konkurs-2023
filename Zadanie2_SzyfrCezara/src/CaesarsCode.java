d1d8e88e8d9ecd36aeb3153085f54b6f99fc86da
Aspink <aspink1@wp.pl>
2023-03-21 13:07:45 +0100

    Method ceasarsCode() created.

--- Zadanie2_SzyfrCezara/src/CaesarsCode.java
+++ Zadanie2_SzyfrCezara/src/CaesarsCode.java
@@ -1,9 +1,17 @@
 public class CaesarsCode {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
     public static String ceasarsCode(int shift, String text) {
-        String codedText = "";
-        return codedText;
+        StringBuilder codedText = new StringBuilder();
+        char[] chars = text.toCharArray();
+        for (char letter : chars) {
+            if(Character.isLetter(letter)) {
+                letter = (char) (((letter + shift - 'a') % 26) + 'a');
+            }
+            codedText.append(letter);
+        }
+        return codedText.toString();
     }
 }
