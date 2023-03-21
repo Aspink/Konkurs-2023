c635ca3f2b1a68b83ff777e6f6687bbfe290d39e
Aspink <aspink1@wp.pl>
2023-03-21 11:38:48 +0100

    Zadanie2_SzyfrCezara and CeasaersCode class created.

---
+++ Zadanie2_SzyfrCezara/src/CaesarsCode.java
@@ -0,0 +1,9 @@
+public class CaesarsCode {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    public static String ceasarsCode(int shift, String text) {
+        String codedText = "";
+        return codedText;
+    }
+}
