a99b40a9d2f6b3a4abd925373bd63c1b546367a4
Aspink <aspink1@wp.pl>
2023-03-21 19:40:05 +0100

    ArrayList alphabet created.

--- Zadanie2_SzyfrCezara/src/CaesarsCode.java
+++ Zadanie2_SzyfrCezara/src/CaesarsCode.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class CaesarsCode {
     public static void main(String[] args) {
+        final Character[] alphabetTab = {'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ź', 'ż', 'A', 'Ą', 'B', 'C', 'Ć', 'D', 'E', 'Ę', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Ł', 'M', 'N', 'Ń', 'O', 'Ó', 'P', 'Q', 'R', 'S', 'Ś', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ź', 'Ż'};
+        ArrayList<Character> alphabet = new ArrayList<>(Arrays.asList(alphabetTab));
+        System.out.println(alphabet);
+        System.out.println(alphabet.size());
         String string = "To zdanie jest zakodowane.";
         int shift1 = 13;
         int shift2 = 6;
