237ae646e2efae97ee153133a246aae091b4f3b4
Aspink <aspink1@wp.pl>
2023-03-22 15:32:28 +0100

    Method convertCtoK created.

--- Zadanie3_KonwerterJednostekTemperatury/src/TemperatureUnitConverter.java
+++ Zadanie3_KonwerterJednostekTemperatury/src/TemperatureUnitConverter.java
@@ -2,4 +2,7 @@ public class TemperatureUnitConverter {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static double convertCtoK(double celsius) {
+        return celsius + 273.15;
+    }
 }
