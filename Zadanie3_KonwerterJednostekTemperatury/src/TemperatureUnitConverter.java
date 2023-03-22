5a9ecb23b59221cf079e71434ff12737315f292c
Aspink <aspink1@wp.pl>
2023-03-22 15:40:17 +0100

    Methods convertKtoC and convertKtoF created.

--- Zadanie3_KonwerterJednostekTemperatury/src/TemperatureUnitConverter.java
+++ Zadanie3_KonwerterJednostekTemperatury/src/TemperatureUnitConverter.java
@@ -8,4 +8,10 @@ public class TemperatureUnitConverter {
     public static double convertCtoF(double celsius) {
         return (celsius * 1.8) + 32;
     }
+    public static double convertKtoC(double kelvin) {
+        return kelvin - 273.15;
+    }
+    public static double convertKtoF(double kelvin) {
+        return convertCtoF(convertKtoC(kelvin));
+    }
 }
