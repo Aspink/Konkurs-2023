f6187cb43e42365a5f2d77fc0e6716a5c955338e
Aspink <aspink1@wp.pl>
2023-03-22 16:11:16 +0100

    Asking of units and repeating created.

--- Zadanie3_KonwerterJednostekTemperatury/src/TemperatureUnitConverter.java
+++ Zadanie3_KonwerterJednostekTemperatury/src/TemperatureUnitConverter.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 public class TemperatureUnitConverter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String again = "T";
+        Scanner scanner = new Scanner(System.in);
+        while(again.equals("T")) {
+            String unit = "";
+            while (!unit.equals("C") && !unit.equals("K") && !unit.equals("F")) {
+                System.out.print("W jakich jednostkach chcesz podać temperaturę? Wybierz C (celsius), K (Kelvin) lub F (Fahrenheit): ");
+                unit = scanner.nextLine().toUpperCase();
+            }
+
+            System.out.print("Czy chcesz dokonać kolejnego przeliczenia? T (tak), dowolny inny (nie): ");
+            again = scanner.nextLine().toUpperCase();
+        }
     }
     public static double convertCtoK(double celsius) {
         return celsius + 273.15;
@@ -13,5 +25,11 @@ public class TemperatureUnitConverter {
     }
     public static double convertKtoF(double kelvin) {
         return convertCtoF(convertKtoC(kelvin));
+    }
+    public static double convertFtoC(double fahrenheit) {
+        return (fahrenheit - 32.0) / 1.8;
+    }
+    public static double convertFtoK(double fahrenheit) {
+        return convertCtoK(convertFtoC(fahrenheit));
     }
 }
