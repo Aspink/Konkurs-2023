e031c6b4806a490cd388357073ac2e32da27b449
Aspink <aspink1@wp.pl>
2023-03-24 21:44:04 +0100

    Zadanie5_SortowaniePrzezWstawianie 05 method randomTab created.

--- Zadanie5_SortowaniePrzezWstawianie/src/InsertionSortTest.java
+++ Zadanie5_SortowaniePrzezWstawianie/src/InsertionSortTest.java
@@ -1,16 +1,18 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class InsertionSortTest {
     private static final List<int[]> testSets = new ArrayList<>();
+    private final Random random = new Random();
 
     public static void main(String[] args) {
-        testSets.add(new int[]{});
+        testSets.add(randomTab(10));
         testSets.add(new int[]{1,2,5,3,1,7,9,1,12,83,1,5,3,2});
         testSets.add(new int[]{1,1,1,1,1,1,1,2,1,1,1});
-        testSets.add(new int[]{2,2,2,2});
-        testSets.add(new int[]{1,2});
+        testSets.add(randomTab(100));
+        testSets.add(randomTab(1000));
         testSets.add(new int[]{2,1});
 
         for (int[] testSet : testSets) {
@@ -45,5 +47,14 @@ public class InsertionSortTest {
             System.out.print("  ");
         }
         System.out.println("\b\b}");
+    }
+
+    public static int[] randomTab(int size) {
+        final Random random = new Random();
+        int[] tab = new int[size];
+        for(int i = 0; i < size; i++) {
+            tab[i] = random.nextInt(size);
+        }
+        return tab;
     }
 }
