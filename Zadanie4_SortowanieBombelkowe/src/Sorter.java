13e96e529c39cac5f308586ae8637651e6fc82e7
Aspink <aspink1@wp.pl>
2023-03-25 22:20:55 +0100

    Zadanie4_SortowanieBombelkowe 06 Method bubbleSorting in Sorted class created.

--- Zadanie4_SortowanieBombelkowe/src/Sorter.java
+++ Zadanie4_SortowanieBombelkowe/src/Sorter.java
@@ -1,4 +1,23 @@
 import java.util.Arrays;
 
 public class Sorter {
+    public static int[] bubbleSorting(int[] unsorted) {
+        int[] sorted = Arrays.copyOf(unsorted, unsorted.length);
+        int unsortedSize = sorted.length - 1;
+        boolean sorting = true;
+        int temp;
+        while(sorting) {
+            sorting = false;
+            for(int i = 0; i < unsortedSize; i++) {
+                if(sorted[i] > sorted[i+1]) {
+                    temp = sorted[i];
+                    sorted[i] = sorted[i+1];
+                    sorted[i+1] = temp;
+                    sorting = true;
+                }
+            }
+            unsortedSize--;
+        }
+        return sorted;
+    }
 }
