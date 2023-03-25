b5bb1fd67e96ff67591fcdbcefc27057f6f8a1d7
Aspink <aspink1@wp.pl>
2023-03-25 22:15:46 +0100

    Zadanie4_SortowanieBombelkowe 03 Main method builded.

--- Zadanie4_SortowanieBombelkowe/src/SorterTester.java
+++ Zadanie4_SortowanieBombelkowe/src/SorterTester.java
@@ -2,7 +2,60 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 public class SorterTester {
+    private static final List<int[]> testSets = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        testSets.add(new int[]{});
+        testSets.add(new int[]{1,2,5,3,1,7,9,1,12,83,1,5,3,2});
+        testSets.add(new int[]{1,1,1,1,1,1,1,2,1,1,1});
+        testSets.add(new int[]{2,2,2,2});
+        testSets.add(new int[]{1,2});
+        testSets.add(new int[]{2,1});
+
+        for (int[] testSet : testSets) {
+            testSorter(testSet);
+        }
+    }
+
+    private static void testSorter(int[] testSet) {
+        System.out.print("Tablica nieposortowana: ");
+        printTable(testSet);
+        int[] sorted = Sorter.bubbleSorting(testSet);
+        int[] comparisonSet = Arrays.stream(testSet)
+                .sorted()
+                .toArray();
+        System.out.print("Tablica posortowana:    ");
+        printTable(sorted);
+        System.out.print("Tablica porównawcza:    ");
+        printTable(comparisonSet);
+        if(isEqual(sorted, comparisonSet)) {
+            System.out.println("\uD83D\uDC9A Test passed\n");
+        }  else {
+            System.out.println("\uD83D\uDC94 Test failed\n");
+        }
+    }
+
+    public static void printTable(int[] table) {
+        System.out.print("{");
+        for(int number : table) {
+            System.out.print(number + ", ");
+        }
+        if(table.length < 1) {
+            System.out.print("  ");
+        }
+        System.out.println("\b\b}");
+    }
+
+    public static boolean isEqual (int[] table1, int[] table2) {
+//        if(table1.length != table2.length) {
+//            return false;
+//        }
+//        for(int i = 0; i<table1.length; i++) {
+//            if(table1[i] != table2[i]) {
+//                return false;
+//            }
+//        }
+//        return true;
+        return Arrays.equals(table1, table2); //replaces commented code above
     }
 }
