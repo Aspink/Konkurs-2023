e7e74ddebcbd75c23e4ad1ebee4765ceb6743a8a
Aspink <aspink1@wp.pl>
2023-03-28 21:34:19 +0200

    Zadanie7_CiagFibonacciego 06 intFibonacci method corected.

--- Zadanie7_CiagFibonacciego/src/FibonacciTest.java
+++ Zadanie7_CiagFibonacciego/src/FibonacciTest.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
+import Fibonacci.Fibonacci;
+
+import static Fibonacci.Fibonacci.*;
 
 public class FibonacciTest {
     public static void main(String[] args) {
         System.out.print("Podaj indeks liczby w ciągu Fibonacciego: ");
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-
+        if(n >= 0 && n <= 10) {
+            System.out.println("" + intFibonacci(n));
+        }
     }
 }
