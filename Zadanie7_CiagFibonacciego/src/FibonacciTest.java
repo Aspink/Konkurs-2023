8cd396157297883fda424a4da5d424eb1ff3ae9a
Aspink <aspink1@wp.pl>
2023-03-28 21:06:57 +0200

    Zadanie7_CiagFibonacciego 03 Input of n created.

---
+++ Zadanie7_CiagFibonacciego/src/FibonacciTest.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class FibonacciTest {
+    public static void main(String[] args) {
+        System.out.print("Podaj indeks liczby w ciągu Fibonacciego: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+    }
+}
