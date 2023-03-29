782f4e37ad7525379f07f2cac5f741e318b1b99c
Aspink <aspink1@wp.pl>
2023-03-29 15:21:07 +0200

    Zadanie7_CiagFibonacciego 09 Method midFibonacci added in Fibonacci class.

--- Zadanie7_CiagFibonacciego/src/Fibonacci/Fibonacci.java
+++ Zadanie7_CiagFibonacciego/src/Fibonacci/Fibonacci.java
@@ -1,5 +1,11 @@
 package Fibonacci;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Fibonacci {
+    public static Map<Integer, BigInteger> results = new HashMap<>();
     public static int intFibonacci(int n) {
         int current = 0, previous = 1, temp;
         for(int i = 0; i < n; i++) {
@@ -8,5 +14,19 @@ public class Fibonacci {
             previous = temp;
         }
         return current;
+    }
+    public static BigInteger midFibonacci(int n) {
+        if(n == 0) {
+            return BigInteger.ZERO;
+        }
+        if(n == 1) {
+            return BigInteger.ONE;
+        }
+        if(results.containsKey(n)) {
+            return results.get(n);
+        }
+        BigInteger result = midFibonacci(n-1).add(midFibonacci(n-2));
+        results.put(n, result);
+        return result;
     }
 }
