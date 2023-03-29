f3bb37c7bf844e3caa6c03572267b35a154290b4
Aspink <aspink1@wp.pl>
2023-03-29 15:37:36 +0200

    Zadanie7_CiagFibonacciego 10 While loop added to calculate next Fibonacci number in FibonacciTest class.

--- Zadanie7_CiagFibonacciego/src/FibonacciTest.java
+++ Zadanie7_CiagFibonacciego/src/FibonacciTest.java
@@ -5,14 +5,20 @@ import static Fibonacci.Fibonacci.*;
 
 public class FibonacciTest {
     public static void main(String[] args) {
-        System.out.print("Podaj indeks liczby w ciągu Fibonacciego: ");
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        if(n >= 0 && n <= 10) {
-            System.out.println(intFibonacci(n));
-        }
-        if(n > 10 && n <= 100) {
-            System.out.println(midFibonacci(n));
+        String again = "T";
+        while(again.equals("T")) {
+            System.out.print("Podaj indeks liczby w ciągu Fibonacciego: ");
+            Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();
+            if(n >= 0 && n <= 10) {
+                System.out.println(intFibonacci(n));
+            }
+            if(n > 10 && n <= 100) {
+                System.out.println(midFibonacci(n));
+            }
+            scanner.nextLine();
+            System.out.print("Czy chcesz dokonać kolejnego obliczenia? T (tak), dowolny inny (nie): ");
+            again = scanner.nextLine().toUpperCase();
         }
     }
 }
