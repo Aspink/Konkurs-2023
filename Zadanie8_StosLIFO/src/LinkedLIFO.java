2b5c417fde59e7edde2cd92cab53def852a5b597
Aspink <aspink1@wp.pl>
2023-03-30 21:48:45 +0200

    Zadanie8_StosLIFO 03 Node private class created.

--- Zadanie8_StosLIFO/src/LinkedLIFO.java
+++ Zadanie8_StosLIFO/src/LinkedLIFO.java
@@ -1,2 +1,12 @@
 public class LinkedLIFO<T> {
+    private Node<T> head;
+    private static class Node<T> {
+        private T data;
+        private Node<T> previous;
+        public Node(T data) {
+            this.data = data;
+            this.previous = null;
+        }
+    }
+
 }
