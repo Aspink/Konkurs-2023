0553563787ecc5a4b8232751622b8c2738f41101
Aspink <aspink1@wp.pl>
2023-03-31 21:10:47 +0200

    Zadanie9_KolejkaFIFO 03 Constructor and data modified.

---
+++ Zadanie9_KolejkaFIFO/src/LinkedFIFO.java
@@ -0,0 +1,54 @@
+public class LinkedFIFO<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public LinkedFIFO() {
+        tail = null;
+        head = null;
+    }
+    public void push(T data) {
+        Node<T> node = new Node<>(data);
+        if(head == null) {
+            head = node;
+        } else {
+            node.previous = head;
+            head = node;
+        }
+    }
+    public T pop() {
+        if (head == null) {
+            return null;
+        }
+        T data = head.data;
+        head = head.previous;
+        return data;
+    }
+    public T peek() {
+        if (head == null) {
+            return null;
+        }
+        return head.data;
+    }
+    public boolean isEmpty() {
+        return head == null;
+    }
+    public int size() {
+        if(head == null) {
+            return 0;
+        }
+        int size = 1;
+        Node current = head;
+        while (current.previous != null) {
+            current = current.previous;
+            size++;
+        }
+        return size;
+    }
+}
