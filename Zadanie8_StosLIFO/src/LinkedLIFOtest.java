faa1bccec748c66eff6bf5d523a522f8a496216b
Aspink <aspink1@wp.pl>
2023-03-31 14:32:30 +0200

    Zadanie8_StosLIFO 12 1 city poped from both stacks. Checking what city is it and what city is curret head. Checking size.

--- Zadanie8_StosLIFO/src/LinkedLIFOtest.java
+++ Zadanie8_StosLIFO/src/LinkedLIFOtest.java
@@ -5,7 +5,7 @@ public class LinkedLIFOtest {
     public static void main(String[] args) {
         LinkedLIFO<String> Cities = new LinkedLIFO<>();
         Deque<String> CitiesJ = new LinkedList<>();
-        System.out.println("Czy mój stos jest pusty? " + Cities.isEmpty() + "\tCzy stos Javy jest pusty? " + CitiesJ.isEmpty());
+        System.out.println("Czy mój stos jest pusty? " + Cities.isEmpty() + "\t\tCzy stos Javy jest pusty? " + CitiesJ.isEmpty());
         Cities.push("Warsaw");
         Cities.push("New York");
         Cities.push("London");
@@ -17,8 +17,13 @@ public class LinkedLIFOtest {
         CitiesJ.push("Pekin");
         CitiesJ.push("Sydney");
         System.out.println("Dodaliśmy 5 miast.");
-        System.out.println("Ile miast jest w moim stosie? " + Cities.size() + "\tIle miast jest w stosie Javy? " + CitiesJ.size());
-        System.out.println("Czy mój stos jest pusty? " + Cities.isEmpty() + "\tCzy stos Javy jest pusty? " + CitiesJ.isEmpty());
+        System.out.println("Ile miast jest w moim stosie? " + Cities.size() + "\t\tIle miast jest w stosie Javy? " + CitiesJ.size());
+        System.out.println("Czy mój stos jest pusty? " + Cities.isEmpty() + "\t\tCzy stos Javy jest pusty? " + CitiesJ.isEmpty());
+        System.out.println("Zabieramy ostatnie miasto. Jakie to miasto?");
+        System.out.println("Mój stos: " + Cities.pop() + "\t\tStos Javy: " + CitiesJ.pop());
+        System.out.println("Jakie miasto jest teraz na szczycie stosu?");
+        System.out.println("Mój stos: " + Cities.peek() + "\t\tStos Javy: " + CitiesJ.peek());
+        System.out.println("Ile miast jest w moim stosie? " + Cities.size() + "\t\tIle miast jest w stosie Javy? " + CitiesJ.size());
 
     }
 }
