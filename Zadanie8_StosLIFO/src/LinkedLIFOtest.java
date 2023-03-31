bb88c0b7c0e24bf93df87e55a995dc4930d71b04
Aspink <aspink1@wp.pl>
2023-03-31 14:24:13 +0200

    Zadanie8_StosLIFO 11 Created stack Cities in my own class LinkedLIFO and Cities J as LinkedList. Checking if stocks are empty. Adding 5 cities to both stacks. Checking size and if stacks are empty now.

--- Zadanie8_StosLIFO/src/LinkedLIFOtest.java
+++ Zadanie8_StosLIFO/src/LinkedLIFOtest.java
@@ -1,6 +1,24 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class LinkedLIFOtest {
     public static void main(String[] args) {
+        LinkedLIFO<String> Cities = new LinkedLIFO<>();
+        Deque<String> CitiesJ = new LinkedList<>();
+        System.out.println("Czy mój stos jest pusty? " + Cities.isEmpty() + "\tCzy stos Javy jest pusty? " + CitiesJ.isEmpty());
+        Cities.push("Warsaw");
+        Cities.push("New York");
+        Cities.push("London");
+        Cities.push("Pekin");
+        Cities.push("Sydney");
+        CitiesJ.push("Warsaw");
+        CitiesJ.push("New York");
+        CitiesJ.push("London");
+        CitiesJ.push("Pekin");
+        CitiesJ.push("Sydney");
+        System.out.println("Dodaliśmy 5 miast.");
+        System.out.println("Ile miast jest w moim stosie? " + Cities.size() + "\tIle miast jest w stosie Javy? " + CitiesJ.size());
+        System.out.println("Czy mój stos jest pusty? " + Cities.isEmpty() + "\tCzy stos Javy jest pusty? " + CitiesJ.isEmpty());
 
-        System.out.println("Hello world!");
     }
 }
