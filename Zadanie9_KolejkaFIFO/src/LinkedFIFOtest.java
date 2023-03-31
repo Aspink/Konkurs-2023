a9400062e81615a517207da6b756ffa4667ca44f
Aspink <aspink1@wp.pl>
2023-03-31 21:42:34 +0200

    Zadanie9_KolejkaFIFO 07 LinkedFIFOtest class modified and tested.

--- Zadanie9_KolejkaFIFO/src/LinkedFIFOtest.java
+++ Zadanie9_KolejkaFIFO/src/LinkedFIFOtest.java
@@ -1,34 +1,34 @@
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class LinkedFIFOtest {
     public static void main(String[] args) {
         LinkedFIFO<String> Cities = new LinkedFIFO<>();
-        Deque<String> CitiesJ = new LinkedList<>();
+        Queue<String> CitiesJ = new ArrayDeque<>();
         System.out.println("Czy moja kolejka jest pusta? " + Cities.isEmpty() + "\t\tCzy kolejka Javy jest pusta? " + CitiesJ.isEmpty());
-        Cities.push("Warsaw");
-        Cities.push("New York");
-        Cities.push("London");
-        Cities.push("Pekin");
-        Cities.push("Sydney");
-        CitiesJ.push("Warsaw");
-        CitiesJ.push("New York");
-        CitiesJ.push("London");
-        CitiesJ.push("Pekin");
-        CitiesJ.push("Sydney");
+        Cities.add("Warsaw");
+        Cities.add("New York");
+        Cities.add("London");
+        Cities.add("Pekin");
+        Cities.add("Sydney");
+        CitiesJ.add("Warsaw");
+        CitiesJ.add("New York");
+        CitiesJ.add("London");
+        CitiesJ.add("Pekin");
+        CitiesJ.add("Sydney");
         System.out.println("Dodaliśmy 5 miast.");
-        System.out.println("Ile miast jest w moim stosie? " + Cities.size() + "\t\tIle miast jest w stosie Javy? " + CitiesJ.size());
-        System.out.println("Czy mój stos jest pusty? " + Cities.isEmpty() + "\t\tCzy stos Javy jest pusty? " + CitiesJ.isEmpty());
-        System.out.println("Zabieramy ostatnie miasto. Jakie to miasto?");
-        System.out.println("Mój stos: " + Cities.pop() + "\t\t\t\t\tStos Javy: " + CitiesJ.pop());
+        System.out.println("Ile miast jest w mojej kolejce? " + Cities.size() + "\t\tIle miast jest w kolejce Javy? " + CitiesJ.size());
+        System.out.println("Czy moja kolejka jest pusta? " + Cities.isEmpty() + "\t\tCzy kolejka Javy jest pusta? " + CitiesJ.isEmpty());
+        System.out.println("Zabieramy pierwsze miasto. Jakie to miasto?");
+        System.out.println("Moja kolejka: " + Cities.poll() + "\t\t\t\t\tKolejka Javy: " + CitiesJ.poll());
         System.out.println("Jakie miasto jest teraz na szczycie stosu?");
-        System.out.println("Mój stos: " + Cities.peek() + "\t\t\t\t\t\tStos Javy: " + CitiesJ.peek());
-        System.out.println("Ile miast jest w moim stosie? " + Cities.size() + "\t\tIle miast jest w stosie Javy? " + CitiesJ.size());
+        System.out.println("Moja kolejka: " + Cities.peek() + "\t\t\t\t\tKolejka Javy: " + CitiesJ.peek());
+        System.out.println("Ile miast jest w mojej kolejce? " + Cities.size() + "\t\tIle miast jest w kolejce Javy? " + CitiesJ.size());
         System.out.println("Zabieramy kolejne miasto. Jakie to miasto?");
-        System.out.println("Mój stos: " + Cities.pop() + "\t\t\t\t\t\tStos Javy: " + CitiesJ.pop());
-        System.out.println("Jakie miasto jest teraz na szczycie stosu?");
-        System.out.println("Mój stos: " + Cities.peek() + "\t\t\t\t\tStos Javy: " + CitiesJ.peek());
-        System.out.println("Ile miast jest w moim stosie? " + Cities.size() + "\t\tIle miast jest w stosie Javy? " + CitiesJ.size());
-        System.out.println("Czy mój stos jest pusty? " + Cities.isEmpty() + "\t\tCzy stos Javy jest pusty? " + CitiesJ.isEmpty());
+        System.out.println("Mój stos: " + Cities.poll() + "\t\t\t\t\t\tKolejka Javy: " + CitiesJ.poll());
+        System.out.println("Jakie miasto jest teraz na początku kolejki?");
+        System.out.println("Moja kolejka: " + Cities.peek() + "\t\t\t\t\tKolejka Javy: " + CitiesJ.peek());
+        System.out.println("Ile miast jest w mojej kolejce? " + Cities.size() + "\t\tIle miast jest w kolejce Javy? " + CitiesJ.size());
+        System.out.println("Czy moja kolejka jest pusta? " + Cities.isEmpty() + "\t\ttCzy kolejka Javy jest pusta? " + CitiesJ.isEmpty());
     }
 }
