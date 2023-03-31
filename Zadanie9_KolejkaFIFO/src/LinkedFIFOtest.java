c4d96065529a2ee42614c5e5c3f47845406ad439
Aspink <aspink1@wp.pl>
2023-03-31 21:30:28 +0200

    Zadanie9_KolejkaFIFO 06 LinkedFIFOtest class with prtotype created.

---
+++ Zadanie9_KolejkaFIFO/src/LinkedFIFOtest.java
@@ -0,0 +1,34 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class LinkedFIFOtest {
+    public static void main(String[] args) {
+        LinkedFIFO<String> Cities = new LinkedFIFO<>();
+        Deque<String> CitiesJ = new LinkedList<>();
+        System.out.println("Czy moja kolejka jest pusta? " + Cities.isEmpty() + "\t\tCzy kolejka Javy jest pusta? " + CitiesJ.isEmpty());
+        Cities.push("Warsaw");
+        Cities.push("New York");
+        Cities.push("London");
+        Cities.push("Pekin");
+        Cities.push("Sydney");
+        CitiesJ.push("Warsaw");
+        CitiesJ.push("New York");
+        CitiesJ.push("London");
+        CitiesJ.push("Pekin");
+        CitiesJ.push("Sydney");
+        System.out.println("Dodaliśmy 5 miast.");
+        System.out.println("Ile miast jest w moim stosie? " + Cities.size() + "\t\tIle miast jest w stosie Javy? " + CitiesJ.size());
+        System.out.println("Czy mój stos jest pusty? " + Cities.isEmpty() + "\t\tCzy stos Javy jest pusty? " + CitiesJ.isEmpty());
+        System.out.println("Zabieramy ostatnie miasto. Jakie to miasto?");
+        System.out.println("Mój stos: " + Cities.pop() + "\t\t\t\t\tStos Javy: " + CitiesJ.pop());
+        System.out.println("Jakie miasto jest teraz na szczycie stosu?");
+        System.out.println("Mój stos: " + Cities.peek() + "\t\t\t\t\t\tStos Javy: " + CitiesJ.peek());
+        System.out.println("Ile miast jest w moim stosie? " + Cities.size() + "\t\tIle miast jest w stosie Javy? " + CitiesJ.size());
+        System.out.println("Zabieramy kolejne miasto. Jakie to miasto?");
+        System.out.println("Mój stos: " + Cities.pop() + "\t\t\t\t\t\tStos Javy: " + CitiesJ.pop());
+        System.out.println("Jakie miasto jest teraz na szczycie stosu?");
+        System.out.println("Mój stos: " + Cities.peek() + "\t\t\t\t\tStos Javy: " + CitiesJ.peek());
+        System.out.println("Ile miast jest w moim stosie? " + Cities.size() + "\t\tIle miast jest w stosie Javy? " + CitiesJ.size());
+        System.out.println("Czy mój stos jest pusty? " + Cities.isEmpty() + "\t\tCzy stos Javy jest pusty? " + CitiesJ.isEmpty());
+    }
+}
