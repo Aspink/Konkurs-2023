7b79eaf516c98d4a6f81a6d1b77ea8e3dc725bb7
Aspink <aspink1@wp.pl>
2023-04-01 21:20:07 +0200

    Zadanie9_KolejkaFIFO 09 Method add created in TabFIFO class.

--- Zadanie9_KolejkaFIFO/src/TabFIFO.java
+++ Zadanie9_KolejkaFIFO/src/TabFIFO.java
@@ -2,6 +2,13 @@ public class TabFIFO<T> {
     private int size = 0;
     private final int MAX_SIZE = 100;
     private Object[] tabQueue = new Object[MAX_SIZE];
+    public void add(T data) {
+        if(size == MAX_SIZE) {
+            System.out.println("Wielkość kolejki przekroczona, nie można dodać elementu.");
+        } else {
+            tabQueue[size++] = data;
+        }
+    }
 
 
 
