968587a5fd266e11f6b92f3b40b7857d01f772c7
Aspink <aspink1@wp.pl>
2023-04-02 21:35:40 +0200

    Zadanie10_ASCII 03 ASCII graphic with text StormIT.pl created.

--- Zadanie10_ASCII/src/AsciiText.java
+++ Zadanie10_ASCII/src/AsciiText.java
@@ -1,5 +1,18 @@
 public class AsciiText {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("\n" +
+                "  SSSSSSSSS     TT                                                          II   TTTTTTTTTTTT                        LL\n" +
+                " SS      SSS    TT          OOOO        RR   RRR   MM  MMMM      MMMM       II        TT            PP    PPPP       LL\n" +
+                "SSS           TTTTTTTT   OOO    OOO     RR RRR     MM MM  MMM  MMM  MMM     II        TT            PP PPP    PPP    LL\n" +
+                " SSSSSS         TT      OO        OO    RRR        MMM      MMMM      MM    II        TT            PPP        PPP   LL\n" +
+                "      SSSSS     TT     OOO         OO   RR         MM        MM       MM    II        TT            PPP         PP   LL\n" +
+                "         SSS    TT      OO        OOO   RR         MM        MM       MM    II        TT            PPP         PP   LL\n" +
+                "SSS      SSS    TT      OOO      OOO    RR         MM        MM       MM    II        TT       **   PPPP       PP    LL\n" +
+                "  SSSSSSSS       TTTT      OOOOOO       RR         MM        MM       MM    II        TT       **   PP  PPPPPPP     LL\n" +
+                "                                                                                                    PP\n" +
+                "                                                                                                    PP\n" +
+                "                                                                                                    PP\n"
+        );
     }
 }
